f648ae5e16c99dc6d87db33459741d49d2b4078c
SalasFacundo <salasfacundo96@gmail.com>
2022-01-13 12:41:51 -0300

    Salto de linea

--- src/main/java/com/sistema/inventario/dao/IHuevoRepository.java
+++ src/main/java/com/sistema/inventario/dao/IHuevoRepository.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sistema.inventario.models.Huevo;
 
 public interface IHuevoRepository extends JpaRepository<Huevo, Integer>{
 	
 	@Query("select dias, count(dias) as cantidad from Huevo group by dias order by dias")
-    List<String> cantidadDias();
+    List<String> cantidadDias();	
+	
+	
+	
+	
 
 }
